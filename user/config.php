13e09d657021a7e709b0596bedb883c52387a243
johniyon <kamalhoussam@gmail.com>
2024-10-10 23:49:38 +0100

    Update config.php

--- user/config.php
+++ user/config.php
@@ -2,5 +2,5 @@ define( 'YOURLS_DB_USER', getenv('YOURLS_DB_USER') );
 define( 'YOURLS_DB_PASS', getenv('YOURLS_DB_PASS') );
 define( 'YOURLS_DB_NAME', getenv('YOURLS_DB_NAME') );
 define( 'YOURLS_DB_HOST', getenv('YOURLS_DB_HOST') );
-
 define( 'YOURLS_SITE', getenv('YOURLS_SITE') );
+define('YOURLS_PRIVATE', false); // Set to false for public access
