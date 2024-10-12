48234180ee45ecabfba70dd034f52a3f89341671
johniyon <kamalhoussam@gmail.com>
2024-10-13 00:28:30 +0100

    Update config.php

--- user/config.php
+++ user/config.php
@@ -1,6 +1,6 @@
-define('DB_USER', 'zm4sneywkm1e4ii3');
-define('DB_PASS', 'wgzqlft1qiox9vxw');
-define('DB_NAME', 'jkl203hmtkw1uzy8');
-define('DB_HOST', 'tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('YOURLS_SITE', 'http://shorthk.herokuapp.com');
+define('DB_USER', 'dbshort_user');
+define('DB_PASS', 'nY49Wr3ttz9NCt5cdSMUT5UrFRdJz2pU');
+define('DB_NAME', 'dbshort');
+define('DB_HOST', 'dpg-cs5fg0d6l47c73f4aqq0-a');
+define('YOURLS_SITE', 'https://shortcute.onrender.com');
 
