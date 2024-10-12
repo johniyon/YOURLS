30e122ba8f321d8a0b1dc0600ba376f8f162b8e9
johniyon <kamalhoussam@gmail.com>
2024-10-12 21:43:26 +0100

    Update config.php

--- user/config.php
+++ user/config.php
@@ -1,4 +1,6 @@
-define('YOURLS_DB_USER', 'your_database_username');
-define('YOURLS_DB_PASS', 'your_database_password');
-define('YOURLS_DB_NAME', 'your_database_name');
-define('YOURLS_DB_HOST', 'your_database_host');
+define('YOURLS_DB_USER', 'b7xq6u4nrro3r1em');
+define('YOURLS_DB_PASS', 'iazuucmta4b7n4y9');
+define('YOURLS_DB_NAME', 'xwtrk021kmr89kgg');
+define('YOURLS_DB_HOST', 'blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('YOURLS_SITE', 'http://shorthk.herokuapp.com');
+
