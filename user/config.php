8819b846c979af47ad922ea0be143f56c8fd70ad
johniyon <kamalhoussam@gmail.com>
2024-10-12 20:45:37 +0100

    Update config.php

--- user/config.php
+++ user/config.php
@@ -1,5 +1,5 @@
 /** MySQL database settings */
-define('YOURLS_DB_USER', getenv('JAWSDB_URL_USER'));  // Your MySQL username
-define('YOURLS_DB_PASS', getenv('JAWSDB_URL_PASSWORD'));  // Your MySQL password
-define('YOURLS_DB_NAME', getenv('JAWSDB_URL_DATABASE'));  // The name of the database
-define('YOURLS_DB_HOST', getenv('JAWSDB_URL_HOST'));      // MySQL host name
+define('YOURLS_DB_USER', getenv('b7xq6u4nrro3r1em'));  // Your MySQL username
+define('YOURLS_DB_PASS', getenv('iazuucmta4b7n4y9'));  // Your MySQL password
+define('YOURLS_DB_NAME', getenv('xwtrk021kmr89kgg'));  // The name of the database
+define('YOURLS_DB_HOST', getenv('blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'));      // MySQL host name
